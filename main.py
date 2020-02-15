38cc0069b1eb3e0a9870db7b3b740a123fab853f
natalydm <natalyd@yandex.ru>
2020-02-15 14:26:10 +0300

    Added main.py

--- main.py
+++ main.py
@@ -1,1 +1,70 @@
-print("Hello world")
+import numpy as np
+def game_core_v1(number):
+    '''ѕросто угадываем на random ни как не использу€ информацию о больше или меньше.
+       ‘ункци€ ѕринимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count+=1
+        predict = np.random.randint(1, 101) # предполагаемое число
+        if number == predict: 
+            break
+    return(count) # выход из цикла, если угадали
+        
+        
+def score_game(game_core_v1):
+    '''«апускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(10000))
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+    score = int(np.mean(count_ls))
+    print(f"¬аш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+# запускаем
+score_game(game_core_v1)
+
+def game_core_v2(number):
+    '''—начала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
+       в зависимости от того, больше оно или меньше нужного.
+       ‘ункци€ принимает загаданное число и возвращает число попыток'''
+    count = 0
+    predict = np.random.randint(1,100)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
+# ѕровер€ем
+score_game(game_core_v2)
+
+
+
+
+def game_core_v3(number):
+    '''»спользуем метод делени€ отрезка пополам'''
+    count = 0
+    left_gr=0
+    right_gr=101
+    predict = int((right_gr-left_gr)/2)
+    while number != predict:
+        
+        count+=1
+        if number > predict: 
+            left_gr=predict
+            predict = int(predict+(right_gr-predict )/ 2)
+                    
+        elif number < predict: 
+            right_gr=predict
+            predict = int(left_gr+(predict- left_gr)/ 2)
+  
+   
+    return(count) # выход из цикла, если угадали
+
+# ѕровер€ем
+score_game(game_core_v3)
